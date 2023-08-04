010fa842a01acc75da680555310e76f82d7120e4
Juan Martin <diazjuanma2505@gmail.com>
2023-08-04 15:01:13 -0300

    Final

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,37 @@
+import {Link} from 'react-router-dom'
+
+
+const Item = ({id, name, img, price, stock}) => {
+
+    return (
+        <div>
+            <article className="card">
+            <picture>
+                <img src={img} className="imgPeque" alt={name}/>
+            </picture>
+            <div className="card-body">
+                <header>
+                    <h5 className="card-title">
+                        {name}
+                    </h5>
+                </header>
+                
+                <section className="card-text">
+                    <p>
+                        Precio :${price}
+                    </p>
+                    <p>
+                        Stock disponible: {stock}
+                    </p>
+                </section>
+                <footer>
+                <Link to={`/item/${id}`} className="btn btn-primary">Ver detalle</Link>
+                </footer>
+            </div>        
+        </article>
+        </div>
+        
+    )
+}
+
+export default Item
